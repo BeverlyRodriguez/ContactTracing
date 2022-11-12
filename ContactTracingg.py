fcf66130919391300e8af110452ccab2629acb5a
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-12 23:58:22 +0800

    Option 1 works with design

--- ContactTracingg.py
+++ ContactTracingg.py
@@ -29,12 +29,13 @@ Options = int(input("\t\nSELECT AN OPTION (Choose from 1-3): "))
 
 while True:
     if Options == 1:
-        print("PLEASE ENTER YOUR PERSONAL DATA")
-        Name = (input("\t\nPLEASE ENTER YOUR FIRST NAME: "))
-        Age = int(input("\tPLEASE ENTER YOUR AGE: "))
-        Gender = (input("\tPLEASE ENTER YOUR GENDER: "))
-        Number = (int(input("\tPLEASE ENTER YOUR NUMBER: ")))
-        CityAddress = (input("\tPLEASE ENTER YOU CITY ADDRESS: "))
+        print("\t\t\t>>>>> PLEASE ENTER YOUR PERSONAL DATA <<<<<")
+        Name = (input("\n\tPLEASE ENTER YOUR FIRST NAME:     "))
+        Age = int(input("\tPLEASE ENTER YOUR AGE:            "))
+        Gender = (input("\tPLEASE ENTER YOUR GENDER:         "))
+        Number = (int(input("\tPLEASE ENTER YOUR NUMBER:         ")))
+        CityAddress = (input("\tPLEASE ENTER YOU CITY ADDRESS:    "))
+        print("\t>>>>> INFORMATIONS ARE SAVED! <<<<<")
 
     elif Options == 2:
         search = input("ENTER THE NAME YOU WANT TO SEARCH: ")
