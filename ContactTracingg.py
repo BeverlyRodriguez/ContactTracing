03156b8c2a839c51f8a299f153cdc25e96c2e5da
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-12 23:48:43 +0800

    Something is Wrong in First Option

--- ContactTracingg.py
+++ ContactTracingg.py
@@ -8,7 +8,7 @@ CityAddress = {}
 
 def menu():
     print("""
-            >>>>> WELCOME TO RODRIGUEZ`S PROGRAM <<<<<
+              >>>>> WELCOME TO RODRIGUEZ`S PROGRAM <<<<<
                  _________________________________
                 ||          MENU OPTIONS          ||
                 ||________________________________||  
@@ -29,15 +29,15 @@ Options = int(input("\t\nSELECT AN OPTION (Choose from 1-3): "))
 
 while True:
     if Options == 1:
-        print("PLEASE ENTER YOUR PERSONAL DATA:")
-        Name = (input("PLEASE ENTER YOUR FIRST NAME: "))
-        Age = int(input("PLEASE ENTER YOUR AGE: "))
-        Gender = (input("PLEASE ENTER YOUR GENDER: "))
-        Number = (int(input("PLEASE ENTER YOUR NUMBER: ")))
-        CityAddress = (input("PLEASE ENTER YOU CITY ADDRESS: "))
+        print("PLEASE ENTER YOUR PERSONAL DATA")
+        Name = (input("\t\nPLEASE ENTER YOUR FIRST NAME: "))
+        Age = int(input("\tPLEASE ENTER YOUR AGE: "))
+        Gender = (input("\tPLEASE ENTER YOUR GENDER: "))
+        Number = (int(input("\tPLEASE ENTER YOUR NUMBER: ")))
+        CityAddress = (input("\tPLEASE ENTER YOU CITY ADDRESS: "))
 
     elif Options == 2:
         search = input("ENTER THE NAME YOU WANT TO SEARCH: ")
         print("RESULTS:")
-        for i in range(len(Name)):
+        
         
