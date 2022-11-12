fce8d85da2f3aa14c7df474e0675ff5699f2968c
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-13 00:35:54 +0800

    Dictionary List New

--- ContactTracingg.py
+++ ContactTracingg.py
@@ -30,15 +30,16 @@ Options = int(input("\t\nSELECT AN OPTION (Choose from 1-3): "))
 while True:
     if Options == 1:
         print("\t\t\t>>>>> PLEASE ENTER YOUR PERSONAL DATA <<<<<")
-        Name = (input("\n\tPLEASE ENTER YOUR FIRST NAME:     "))
+        Name = (input("\n\tPLEASE ENTER YOUR FULL NAME:     "))
         Age = int(input("\tPLEASE ENTER YOUR AGE:            "))
         Gender = (input("\tPLEASE ENTER YOUR GENDER:         "))
         Number = (int(input("\tPLEASE ENTER YOUR NUMBER:         ")))
         CityAddress = (input("\tPLEASE ENTER YOU CITY ADDRESS:    "))
         print("\t>>>>> INFORMATIONS ARE SAVED! <<<<<")
+        
 
     elif Options == 2:
         search = input("ENTER THE NAME YOU WANT TO SEARCH: ")
-        print("RESULTS:")
+        for i in range(len(Name)):
         
         
