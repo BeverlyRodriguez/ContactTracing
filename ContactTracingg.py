11ce72a73e9f5f17b325290241116835e934d6dc
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-12 22:28:03 +0800

    Def menu, def end

--- ContactTracingg.py
+++ ContactTracingg.py
@@ -1,11 +1,14 @@
-print("""
-        >>>>> WELCOME TO RODRIGUEZ`S PROGRAM <<<<<
-              _________________________________
-            ||           MENU OPTIONS         ||
-            ||________________________________||  
-            ||                                ||
-            ||         1. ADD AN ITEM         ||
-            ||         2. SEARCH              ||
-            ||         3. EXIT                ||
-            ||________________________________||
-    """)
+def menu():
+    print("""
+            >>>>> WELCOME TO RODRIGUEZ`S PROGRAM <<<<<
+                _________________________________
+                ||           MENU OPTIONS         ||
+                ||________________________________||  
+                ||                                ||
+                ||         1. ADD AN ITEM         ||
+                ||         2. SEARCH              ||
+                ||         3. EXIT                ||
+                ||________________________________||
+        """)
+def end():
+    print(">>>>> THANK YOU FOR USING THIS PROGRAM! <<<<<")
