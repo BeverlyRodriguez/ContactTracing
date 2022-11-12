3fa60c8b0eb810e0e7a1ad8dad8d60688a980679
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-12 18:27:06 +0800

    Displaying a Menu

---
+++ ContactTracing.py
@@ -0,0 +1,30 @@
+print("\n\t\t********PROGRAMMED BY:********")
+print("\t\t***BEVERLY ANN L. RODRIGUEZ***\n")
+
+
+#Write a python program for contact tracing:
+# Display a menu of options
+# Allow user to select item in the menu.
+# Ask personal data for contact tracing. Use dictionary to store the info
+# Search, ask full name then display the record
+
+
+
+def menu(): 
+    print("""
+        >>>>> WELCOME TO RODRIGUEZ`S PROGRAM <<<<<
+             _________________________________
+            |           MENU OPTIONS          |
+            |_________________________________|  
+            |                                 |
+            |         1. ADD AN ITEM          |
+            |         2. SEARCH               |
+            |         3. EXIT                 |
+            |_________________________________|
+    """)
+
+
+# Lists
+FullName = []
+Age = []
+Address = []
