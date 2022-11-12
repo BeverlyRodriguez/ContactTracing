d02ad40ef62c590333af9d834ebc3e91b2076d03
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-13 01:16:08 +0800

    Sorting Error/Finding Solutions

--- ContactTracingg.py
+++ ContactTracingg.py
@@ -1,29 +1,25 @@
 print("\n\t\t********PROGRAMMED BY:********")
 print("\t\t***BEVERLY ANN L. RODRIGUEZ***\n")
 
-Person1 = {"\nFullName": "Bebs Reyes", 
-            "\nAge": "19", 
-            "\nGender":"Female",
-            "\nNumber": "09283623123",
-            "\nEmail": "beverly@gmail.com",
-            "\nAddress": "Jalajala"}
-Person2 = {"\nFullName": "Jay Park", 
-            "\nAge": "20",
-            "\nGender":"Male","Number": "09123985458",
-            "\nEmail": "jaypark@gmail.com",
-            "\nAddress": "Seoul"}
-Person3 = {"\nFullName": "Niki Rodriguez", 
-            "\nAge": "17", 
-            "\nGender":"Male",
-            "\nNumber": "09096937644",
-            "\nEmail": "nikirodriguez@gmail.com",
-            "\nAddress": "Tokyo"}
-Person4 = {"\nFullName": "Jake Marcus",
-            "\nAge": "19", 
-            "\nGender":"Male",
-            "\nNumber": "09845321473",
-            "\nEmail": "jakemarcus@gmail.com",
-            "\nAddress": "Makati"}
+Person1 = {"FullName": "Bebs Reyes", 
+            "Age": "19", 
+            "Gender":"Female",
+            "Number": "09283623123",
+            "Address": "Jalajala"}
+Person2 = {"FullName": "Jay Park", 
+            "\ge": "20",
+            "Gender":"Male","Number": "09123985458",
+            "Address": "Seoul"}
+Person3 = {"FullName": "Niki Rodriguez", 
+            "Age": "17", 
+            "Gender":"Male",
+            "Number": "09096937644",
+            "Address": "Tokyo"}
+Person4 = {"FullName": "Jake Marcus",
+            "Age": "19", 
+            "Gender":"Male",
+            "Number": "09845321473",
+            "Address": "Makati"}
 Informations = []
 
 
@@ -46,20 +42,21 @@ def end():
 
 
 menu()
-Options = int(input("\t\nSELECT AN OPTION (Choose from 1-3): "))
+Options = int(input("\t\nSELECT AN OPTION (Choose from 1-4): "))
 
 while True:
     if Options == 1:
         print("\t\t\t>>>>> PLEASE ENTER YOUR PERSONAL DATA <<<<<")
-        Name = (input("\n\tPLEASE ENTER YOUR FULL NAME:     "))
+        Name = (input("\n\tPLEASE ENTER YOUR FULL NAME:      "))
         Age = int(input("\tPLEASE ENTER YOUR AGE:            "))
         Gender = (input("\tPLEASE ENTER YOUR GENDER:         "))
         PhoneNumber = (int(input("\tPLEASE ENTER YOUR NUMBER:         ")))
         CityAddress = (input("\tPLEASE ENTER YOU CITY ADDRESS:    "))
         print("\t>>>>> INFORMATIONS ARE SAVED! <<<<<")
-        person = {"Fullname": Name, "Age": Age, "Gender": Gender, "Address": CityAddress, "Number": PhoneNumber}
-
+        person = {"\nFullname": Name, "\nAge": Age, "\nGender": Gender, "\nAddress": CityAddress, "\nNumber": PhoneNumber}
+        print("This is the informations ", person)
         Informations.append(Person1)
+
         Informations.append(Person2)
         Informations.append(Person3)
         Informations.append(Person4)
@@ -69,13 +66,20 @@ while True:
         for i in range(len(Informations)):
             person = Informations[i]
             if person["Fullname"] == Name:
-                print()
+                print("person [")
                 print(person)
 
-    elif Options == 4:   
+    elif Options == 3:   
         for i in range(len(Informations)):
+            print("\t\t\t>>>>> THESE ARE ALL THE INFORMATIONS <<<<<")
             print(Informations[i])
+
+    elif Options == 4:
+        exit = input("DO YOU WANT TO EXIT THE PROGRAM? (YES / NO)")
+        if exit == "YES":
+            break
     menu()
     Options = int(input("\t\nSELECT AN OPTION (Choose from 1-4): "))
+
         
         
